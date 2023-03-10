e851ce840f161d13d696125fd7cb9b517fa2853e
Bharath Raj <bharathrajgngowdara@gmail.com>
2023-03-10 18:11:08 +0530

    feat: add routes as constants

---
+++ src/constants/routes.js
@@ -0,0 +1,6 @@
+export const HOME_ROUTE = '/';
+export const COLLECTION_ROUTE = '/collection';
+export const SIGN_IN_ROUTE = '/login';
+export const SIGN_UP_ROUTE = '/register';
+export const ERROR_ROUTE = '/error';
+export const NOT_FOUND_ROUTE = '*';
